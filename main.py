c9aaa7c060441537186e79e4f4d4d2b8fdb5fdf5
news <news@vfwq.net>
2019-08-06 17:02:07 +0800

    调整参数

--- main.py
+++ main.py
@@ -21,24 +21,27 @@ def regular():
     i=0
     while True:
         time.sleep(0.01)
-        if poco(text="[微信红包]恭喜发财，大吉大利"):
-            poco(text="[微信红包]恭喜发财，大吉大利").click()
-            clickhongbao()
-            i=i+1
-            print(i)
-        elif poco(textMatches="^.*微信红包.*$"):
-            poco(textMatches="^.*微信红包.*$").click()
-            clickhongbao()
-            i=i+1
-            print(i)
+        try:
+            if poco(text="[微信红包]恭喜发财，大吉大利"):
+                poco(text="[微信红包]恭喜发财，大吉大利").click()
+                clickhongbao()
+                i=i+1
+                print(i)
+            elif poco(textMatches="^.*微信红包.*$"):
+                poco(textMatches="^.*微信红包.*$").click()
+                clickhongbao()
+                i=i+1
+                print(i)
+        except:
+            pass
         # poco().swipe([0.0, 0.0682]) #未开启充电防休眠模式时启用，自动点击屏幕，防止休眠
 
 
 def clickhongbao():
-    if poco("com.tencent.mm:id/as_"):
-        poco("com.tencent.mm:id/as_")[len(poco("com.tencent.mm:id/as_"))-1].click()
-        if poco("com.tencent.mm:id/d4h"):
-            poco("com.tencent.mm:id/d4h").click()
+    if poco("com.tencent.mm:id/asl"):
+        poco("com.tencent.mm:id/asl")[-1].click()
+        if poco("com.tencent.mm:id/d5a"):
+            poco("com.tencent.mm:id/d5a").click()
         elif poco("com.tencent.mm:id/d4j"):
             poco("com.tencent.mm:id/d4j").click()
             time.sleep(1)
